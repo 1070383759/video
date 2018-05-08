5076cff1962db9a29c494021af3f15e696ed9e4c
jason <1070383759@qq.com>
2018-05-08 19:41:25 +0800

    swoole直播

---
+++ server.php
@@ -0,0 +1,9 @@
+<?php
+$server = new swoole_websocket_server("0.0.0.0", 8080);
+$server->on('message', function (swoole_websocket_server $server, $frame) {
+	foreach($server->connection_list() as $fd) {
+		$server->push($fd, $frame->data,true);
+	}
+});
+$server->start();
+
